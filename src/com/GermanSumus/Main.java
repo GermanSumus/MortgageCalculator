51bce78d8068355e13618e1f0da61d3e00367da7
German Rodriguez <GermanRodriguezBusiness@gmail.com>
2021-02-11 19:04:04 -0600

    simple control flow implemented, errors still possible if wrong data type submitted

--- src/com/GermanSumus/Main.java
+++ src/com/GermanSumus/Main.java
@@ -11,21 +11,47 @@ public class Main {
     // Print out mortgage payment.
         final byte TO_PERCENT = 100;
         final byte MONTHS_IN_YEAR = 12;
+        float principal;
+        float apr;
+        int time;
 
         Scanner scanner = new Scanner(System.in);
         NumberFormat currency = NumberFormat.getCurrencyInstance();
 
-        System.out.print("Mortgage Amount: ");
-        float principal = Float.parseFloat(scanner.nextLine());
-        System.out.print("Annual Percentage Rate: ");
-        float apr = Float.parseFloat(scanner.nextLine())/TO_PERCENT;
-        System.out.print("Years: ");
-        int time = Integer.parseInt(scanner.nextLine())*MONTHS_IN_YEAR;
+        while (true) {
+            System.out.print("Mortgage Amount: ");
+            principal = Float.parseFloat(scanner.nextLine());
+            if ((principal >= 1_000) && (principal <= 1_000_000))
+                break;
+            else
+                System.out.println("Enter value between 1K and 1M.");
+        }
+
+        while (true) {
+            System.out.print("Annual Percentage Rate: ");
+            apr = Float.parseFloat(scanner.nextLine()) / TO_PERCENT;
+            if ((apr > 0) && (apr <= 30))
+                break;
+            else
+                System.out.println("Enter value between 0 and 30");
+        }
+
+        while (true) {
+            System.out.print("Years: ");
+            time = Integer.parseInt(scanner.nextLine());
+            if ((time > 0) && (time <= 30)) {
+                time = time * MONTHS_IN_YEAR;
+                break;
+            }
+            else
+                System.out.println("Enter value between 0 and 30");
+        }
 
         float rate = apr/MONTHS_IN_YEAR;
         double x = principal * rate * Math.pow((1+rate), time);
         double y = Math.pow((1+rate), time) - 1;
         double mortgagePayment =  x/y;
+
         System.out.println(currency.format(mortgagePayment));
     }
 }
